e8c89c9c28f2546ed5a55bc01fb8e4b64bdfd94b
Baseng0815 <1shedex2@gmail.com>
2019-11-07 15:09:03 +0100

    Added camera

---
+++ Camera.cpp
@@ -0,0 +1,81 @@
+#include "pch.h"
+
+#include "Camera.h"
+
+void Camera::recalculateVectors() {
+	glm::vec3 front;
+	front.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+	front.y = sin(glm::radians(m_pitch));
+	front.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+	m_front = glm::normalize(front);
+	m_right = glm::normalize(glm::cross(m_front, glm::vec3(0, 1, 0)));
+	m_up = glm::normalize(glm::cross(m_right, m_front));
+}
+
+Camera::Camera() {}
+
+Camera::Camera(int width, int height, float fov) 
+	: m_width(width), m_height(height), m_fov(fov) {
+
+	recalculateProjectionMatrix(width, height);
+	recalculateVectors();
+}
+
+glm::mat4 Camera::getProjectionMatrix() const {
+	return m_projectionMatrix;
+}
+
+glm::mat4 Camera::getViewMatrix() const {
+	return m_viewMatrix;
+}
+
+void Camera::recalculateProjectionMatrix(int width, int height) {
+	m_width = width;
+	m_height = height;
+
+	m_projectionMatrix = glm::perspective(glm::radians(m_fov), width / (float)height, 0.1f, 1000.f);
+}
+
+void Camera::handleKeys(const Window& window, int dt) {
+	float velocity = dt * MOVE_SPEED;
+
+	// L-CTRL used for speed up
+	if (window.getKey(GLFW_KEY_LEFT_CONTROL)) velocity *= 10;
+
+	if (window.getKey(GLFW_KEY_SPACE))
+		m_position += velocity * glm::vec3(0, 1, 0);
+	if (window.getKey(GLFW_KEY_LEFT_SHIFT))
+		m_position += velocity * glm::vec3(0, -1, 0);
+	if (window.getKey(GLFW_KEY_D))
+		m_position += velocity * m_right;
+	if (window.getKey(GLFW_KEY_A))
+		m_position += velocity * -m_right;
+	if (window.getKey(GLFW_KEY_W))
+		m_position += velocity * m_front;
+	if (window.getKey(GLFW_KEY_S))
+		m_position += velocity * -m_front;
+
+	recalculateVectors();
+}
+
+void Camera::handleMouseMotion(float dx, float dy) {
+	dx *= MOUSE_SENSITIVITY;
+	dy *= MOUSE_SENSITIVITY;
+
+	m_yaw += dx;
+	m_pitch -= dy;
+
+	if (m_pitch > 89) m_pitch = 89;
+	else if (m_pitch < -89) m_pitch = -89;
+
+	recalculateVectors();
+}
+
+void Camera::handleMouseScroll(int dy) {
+	m_fov -= dy;
+
+	if (m_fov > 179) m_fov = 179;
+	else if (m_fov < 1) m_fov = 1;
+
+	recalculateProjectionMatrix(m_width, m_height);
+}
---
+++ Camera.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "pch.h"
+
+#include "Window.h"
+
+class Camera {
+private:
+	float m_yaw, m_pitch, m_fov;
+	int m_width, m_height;
+	
+	const float MOUSE_SENSITIVITY = 0.2f;
+	const float MOUSE_SCROLL = 2.0f;
+	const float MOVE_SPEED = 2.8f;
+
+	glm::vec3 m_position, m_front, m_up, m_right;
+	glm::mat4 m_projectionMatrix, m_viewMatrix;
+
+	// recalculate view matrix, front, up and right vectors
+	// based on yaw and pitch
+	void recalculateVectors();
+
+public:
+	Camera();
+	Camera(int width, int height, float fov);
+
+	glm::mat4 getProjectionMatrix() const;
+	glm::mat4 getViewMatrix() const;
+
+	void recalculateProjectionMatrix(int width, int height);
+
+	// key handler (not event based)
+	// window as argument to retrieve current key states
+	// dt always in milliseconds
+	void handleKeys(const Window& window, int dt);
+
+	void handleMouseMotion(float dx, float dy);
+
+	void handleMouseScroll(int dy);
+};
