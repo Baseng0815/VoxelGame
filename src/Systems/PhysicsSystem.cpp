00fd42e834f7b07ba3c939ea336279e447fb061a
PhiGei2000 <Philipp-Geil@outlook.com>
2020-08-03 18:04:14 +0200

    Add enter chunk event

--- src/Systems/PhysicsSystem.cpp
+++ src/Systems/PhysicsSystem.cpp
@@ -4,6 +4,9 @@
 #include "../../include/Components/TransformationComponent.h"
 #include "../../include/Components/VelocityComponent.h"
 
+#include "../../include/Configuration.h"
+#include "../../include/Events/EventDispatcher.h"
+
 #include <iostream>
 
 PhysicsSystem::PhysicsSystem(entt::registry* registry)
@@ -21,5 +24,21 @@ void PhysicsSystem::_update(int millis) {
 }
 
 void PhysicsSystem::updatePlayer(PlayerComponent& player, TransformationComponent& transform, VelocityComponent& velocity, float dt) {
+    glm::vec3 oldPlayerPos = transform.getPosition();
+    glm::ivec2 oldChunk = glm::ivec2(
+        (int)oldPlayerPos.x / CHUNK_SIZE,
+        (int)oldPlayerPos.z / CHUNK_SIZE
+    );
+    
     transform.move(dt * velocity.velocity);    
+    glm::vec3 newPlayerPos = transform.getPosition();
+    glm::ivec2 newChunk = glm::ivec2(
+        (int)newPlayerPos.x / CHUNK_SIZE,
+        (int)newPlayerPos.z / CHUNK_SIZE
+    );
+    
+    if(newChunk != oldChunk)    {
+        EnterChunkEvent e = EnterChunkEvent(nullptr, oldChunk.x, oldChunk.y, newChunk.x, newChunk.y);
+        EventDispatcher::raiseEvent(e);
+    }
 }
