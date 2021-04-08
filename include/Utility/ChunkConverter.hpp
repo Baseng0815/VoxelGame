564036e27e96c9296c6c41b19baba09d7ad0618c
PhiGei2000 <Philipp-Geil@outlook.com>
2021-04-08 19:43:26 +0200

    Implement ChunkSection to Chunk conversion

--- include/Utility/ChunkConverter.hpp
+++ include/Utility/ChunkConverter.hpp
@@ -11,10 +11,15 @@ struct ChunkSection;
 namespace Utility {
     class ChunkConverter {
       private:
+        static constexpr int sectionsCount = Configuration::CHUNK_HEIGHT / Configuration::CHUNK_SIZE;
+
         static ChunkSection getChunkSection(const ChunkComponent& chunk, const glm::ivec3& pos, int size);
+
+        static void fillBlocks(BlockId*** blocks, const ChunkSection& section);
 
       public:
         static std::vector<ChunkSection> chunkToSections(const ChunkComponent& chunk);
 
+        static BlockId*** sectionsToChunk(const std::vector<ChunkSection>& sections);
     };
 } // namespace Utility
--- src/Utility/ChunkConverter.cpp
+++ src/Utility/ChunkConverter.cpp
@@ -24,15 +24,57 @@ namespace Utility {
         return section;
     }
 
+    void ChunkConverter::fillBlocks(BlockId*** blocks, const ChunkSection& section) {
+        if (section.isAir()) {
+            return;
+        }
+
+        if (!section.hasSubsections()) {
+            BlockId block = section.blockType;
+
+            glm::ivec3 start = section.position;
+            glm::ivec3 end = start + (int)section.size * glm::ivec3{1};
+
+            for (int x = start.x; x < end.x; x++) {
+                for (int y = start.y; y < end.y; y++) {
+                    for (int z = start.z; z < end.z; z++) {
+                        blocks[x][y][z] = block;
+                    }
+                }
+            }
+        }
+        else {
+            int halfSize = (int)section.size / 2;
+            for (int i = 0; i < ChunkSection::SUBSECTIONS_COUNT; i++) {
+                const ChunkSection& subsection = section.subsections[i];
+                fillBlocks(blocks, subsection);
+            }
+        }
+    }
+
     std::vector<ChunkSection> ChunkConverter::chunkToSections(const ChunkComponent& chunk) {
-        constexpr int sectionsCount = Configuration::CHUNK_HEIGHT / Configuration::CHUNK_SIZE;
         std::vector<ChunkSection> sections{sectionsCount};
 
         for (int i = 0; i < sectionsCount; i++) {
             sections.emplace_back(getChunkSection(chunk, Configuration::CHUNK_SIZE * i * glm::ivec3{0, 1, 0}, Configuration::CHUNK_SIZE));
         }
 
-        sections.shrink_to_fit();
         return sections;
+    }
+
+    BlockId*** ChunkConverter::sectionsToChunk(const std::vector<ChunkSection>& sections) {
+        BlockId*** blocks = new BlockId**[Configuration::CHUNK_SIZE];
+        for (int x = 0; x < Configuration::CHUNK_SIZE; x++) {
+            blocks[x] = new BlockId*[Configuration::CHUNK_HEIGHT];
+            for (int y = 0; y < Configuration::CHUNK_HEIGHT; y++) {
+                blocks[x][y] = new BlockId[Configuration::CHUNK_SIZE];
+            }
+        }
+
+        for (const auto& section : sections) {
+            fillBlocks(blocks, section);
+        }
+
+        return blocks;
     }
 } // namespace Utility
